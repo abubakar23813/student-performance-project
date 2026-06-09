ee0738393ec4b874dae2a46541ed0f7b06d6b0e8
abubakar23813 <akqadri812@gmail.com>
2026-06-09 14:14:04 +0530

    Data transformation done

---
+++ src/components/data_transformation.py
@@ -0,0 +1,133 @@
+import sys
+import os
+from dataclasses import dataclass
+ 
+
+import numpy as np
+import pandas as pd
+
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+
+from src.utils import save_object
+
+
+target_column_name="math_score"
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join("artifacts", "preprocessor.pkl")
+
+
+class DataTransformation:
+    def __init__(self):
+        self.data_tranformation_config=DataTransformationConfig()
+        
+    
+    def get_data_transformer_object(self):
+        """This Function Is Responsible For Data transformation..."""
+        try:
+            numerical_columns=["writing_score","reading_score"]
+
+            categorical_columns=[
+                "gender",
+                "race_ethnicity",
+                "parental_level_of_education",
+                "lunch",
+                "test_preparation_course"
+            ]
+
+            num_pipeline=Pipeline(
+                steps=[("imputer",SimpleImputer(strategy="median")),
+                       ("scaler",StandardScaler())]
+
+            )
+
+
+            cat_pipeline=Pipeline(
+                steps=[
+
+                    ("imputer",SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder",OneHotEncoder(handle_unknown="ignore")),
+                    ("scaler",StandardScaler(with_mean=False))
+                ]
+
+            )
+
+
+            logging.info(f"numerical columns: {numerical_columns}")
+            logging.info(f"categorical columns:{categorical_columns}")
+
+
+            preprocessor=ColumnTransformer(
+                [
+                    ("num_pipeline",num_pipeline,numerical_columns),
+                    ("cat_pipeline",cat_pipeline,categorical_columns)
+                ]
+            )
+            
+
+            return preprocessor
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def intiate_data_transformation(self,train_path,test_path):
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+
+
+            logging.info("read the train & test data ")
+
+            logging.info("obtaining preprocessing object")
+
+
+            preprocessing_obj=self.get_data_transformer_object()
+
+            targeted_column=target_column_name
+            
+
+            input_feature_train_df=train_df.drop(columns=[targeted_column])
+            targeted_feature_train_df=train_df[targeted_column]
+
+            input_feature_test_df=test_df.drop(columns=[targeted_column])
+            targeted_feature_test_df=test_df[targeted_column]
+
+
+            logging.info(
+                "Applying preprocessing object on training dataframe and testing dataframe"
+            )
+
+            input_feature_train_array=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_array=preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr=np.c_[
+                input_feature_train_array,np.array(targeted_feature_train_df)
+            ]
+
+            test_arr=np.c_[
+                input_feature_test_array,np.array(targeted_feature_test_df)
+            ]
+
+            logging.info("saved preprocessing object")
+
+            save_object(
+                file_path=self.data_tranformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+
+            return(
+                train_arr,
+                test_arr,
+                self.data_tranformation_config.preprocessor_obj_file_path,
+            )
+
+        except Exception as e:
+            raise CustomException(e,sys)
+            
+            
